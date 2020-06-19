a011b390f38ffe27a5067929e84fd7b44dd9e02d
zhanglinyao <zhanglinyao@yunzhangfang.com>
2020-06-19 17:27:00 +0800

    增加罗马转数字

--- src/com/jba/learn/algorithm/leetCode/IntToRoman.java
+++ src/com/jba/learn/algorithm/leetCode/IntToRoman.java
@@ -1,11 +1,14 @@
 package com.jba.learn.algorithm.leetCode;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
 
 /**
- * 罗马数字转整数
+ * 整数转罗马数字
  * <pre>
- *   罗马数字包含以下七种字符: I， V， X， L，C，D 和 M。
+ *   罗马数字包含以下七种字符： I， V， X， L，C，D 和 M。
  *
  * 字符          数值
  * I             1
@@ -22,65 +25,57 @@ import java.util.HashMap;
  * I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
  * X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
  * C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
- * 给定一个罗马数字，将其转换成整数。输入确保在 1 到 3999 的范围内。
+ * 给定一个整数，将其转为罗马数字。输入确保在 1 到 3999 的范围内。
  *
  * 示例 1:
  *
- * 输入: "III"
- * 输出: 3
+ * 输入: 3
+ * 输出: "III"
  * 示例 2:
  *
- * 输入: "IV"
- * 输出: 4
+ * 输入: 4
+ * 输出: "IV"
  * 示例 3:
  *
- * 输入: "IX"
- * 输出: 9
+ * 输入: 9
+ * 输出: "IX"
  * 示例 4:
  *
- * 输入: "LVIII"
- * 输出: 58
- * 解释: L = 50, V= 5, III = 3.
+ * 输入: 58
+ * 输出: "LVIII"
+ * 解释: L = 50, V = 5, III = 3.
  * 示例 5:
  *
- * 输入: "MCMXCIV"
- * 输出: 1994
+ * 输入: 1994
+ * 输出: "MCMXCIV"
  * 解释: M = 1000, CM = 900, XC = 90, IV = 4.
  *
  * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/roman-to-integer
+ * 链接：https://leetcode-cn.com/problems/integer-to-roman
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  * <pre/>
  */
-public class RomanToInt {
+public class IntToRoman {
 
+  int[] dict = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+  String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-  public static int romanToInt(String s) {
-    HashMap<Character, Integer> dict = new HashMap<>();
-    dict.put('I', 1);
-    dict.put('V', 5);
-    dict.put('X', 10);
-    dict.put('L', 50);
-    dict.put('C', 100);
-    dict.put('D', 500);
-    dict.put('M', 1000);
-    int result = 0;
-    char[] chars = s.toCharArray();
-    int prev = dict.get(chars[0]);
-    for (int i = 1; i < chars.length; i++) {
-      int cur = dict.get(chars[i]);
-      if (prev < cur) {
-        result -= prev;
-      } else {
-        result += prev;
-      }
-      prev = cur;
+  public String intToRoman(int num) {
+    if (num <= 0) {
+      return "";
     }
-    result += prev;
-    return result;
+    StringBuilder roman = new StringBuilder();
+    for (int i = 0; i < dict.length; i++) {
+      while (dict[i] <= num) {
+        num = num - dict[i];
+        roman.append(symbols[i]);
+      }
+    }
+    return roman.toString();
   }
 
   public static void main(String[] args) {
-    System.out.println(romanToInt("LXXX"));
+    IntToRoman intToRoman = new IntToRoman();
+    System.out.println(intToRoman.intToRoman(500));
   }
 }
