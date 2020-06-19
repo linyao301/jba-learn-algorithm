06980ca3d40625e2c5cd86bc4c01df626a733688
zhanglinyao <zhanglinyao@yunzhangfang.com>
2020-06-19 11:47:53 +0800

    增加三数之和的算法题解

--- src/com/jba/learn/algorithm/leetCode/ThreeSum.java
+++ src/com/jba/learn/algorithm/leetCode/ThreeSum.java
@@ -1,9 +1,8 @@
 package com.jba.learn.algorithm.leetCode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
-import javax.swing.ListSelectionModel;
 
 /**
  * 三数之和
@@ -31,21 +30,58 @@ import javax.swing.ListSelectionModel;
  */
 public class ThreeSum {
 
+  /**
+   * <pre>
+   *   在排序之后重复需要达到三个条件
+   *   1:当前元素和上一个元素一致需要排除
+   *   2：当sum=0时，[Left]=[Left+1]需要跳过[Left+1]
+   *   3：当sum=0是，[RIGHT]=[RIGHT-1]需要跳过[RIGHT+1]
+   * </pre>
+   */
   public static List<List<Integer>> threeSum(int[] nums) {
     List<List<Integer>> result = new ArrayList<>();
-    for (int i = 0; i < nums.length - 2; i++) {
-      int a1 = nums[i];
-      for (int j = i + 1; j < nums.length - 1; j++) {
-        int a2 = nums[j];
-        for (int z = j + 1; z < nums.length; z++) {
-          int a3 = nums[z];
-          if (a1 + a2 + a3 == 0) {
-            List<Integer> match = new ArrayList<>();
-            match.add(a1);
-            match.add(a2);
-            match.add(a3);
-            result.add(match);
+    //为null时直接返回
+    if (nums == null) {
+      return result;
+    }
+    int len = nums.length;
+    //长度小于3时直接返回
+    if (len < 3) {
+      return result;
+    }
+    Arrays.sort(nums);
+    for (int i = 0; i < len - 2; i++) {
+      //因已经排序，如果当前元素>0直接退出
+      if (nums[i] > 0) {
+        break;
+      }
+      //如果当前元素=前一个元素，避免重复跳过
+      if (i > 0 && nums[i] == nums[i - 1]) {
+        continue;
+      }
+      int left = i + 1, right = len - 1;
+      while (left < right) {
+        int sum = nums[i] + nums[left] + nums[right];
+        if (sum == 0) {
+          List<Integer> ans = new ArrayList<>();
+          ans.add(nums[i]);
+          ans.add(nums[left]);
+          ans.add(nums[right]);
+          result.add(ans);
+          //当sum=0时，寻找左元素!=当前左元素的数，避免重复
+          while (left < right && nums[left] == nums[left + 1]) {
+            left++;
           }
+          //当sum=0时，需要右元素!=当前右元素，避免重复
+          while (left < right && nums[right] == nums[right - 1]) {
+            right--;
+          }
+          left++;
+          right--;
+        } else if (sum < 0) {
+          left++;
+        } else if (sum > 0) {
+          right--;
         }
       }
     }
@@ -53,7 +89,7 @@ public class ThreeSum {
   }
 
   public static void main(String[] args) {
-    List<List<Integer>> result = threeSum(new int[]{-1, 0, 1, 2, -1, -4});
+    List<List<Integer>> result = threeSum(new int[]{-1, 0, 1, 2, 2, 2, -1, -4});
     System.out.println("OK");
   }
 }
